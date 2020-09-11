5f76c636cc6ee7b52e100fa26285c349b8ba5893
nacho11 <jisalas1@uc.cl>
2020-09-11 10:59:19 -0300

    :sparkles: Handle arguments

--- src/edf/main.c
+++ src/edf/main.c
@@ -1,4 +1,35 @@
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+
+void run(char *file, char *output, int n_cpu)
 {
+  printf("file: %s output: %s cpu: %i \n", file, output, n_cpu);
+}
+
+int main(int argc, char **argv)
+{
+  int n_cpu;
+
+  if (argc < 3) // Los argumentos no fueron pasado
+  {
+    printf("Número de parámetros incorrectos:\n\n");
+    printf("./edf <file> <output> [<n>]\n");
+    printf("- <file>:   Archivo de entrada\n");
+    printf("- <output>: Ruta de sálida para un csv\n");
+    printf("- <n>:      Cantidad de CPUs. Default=1\n");
+    return 0;
+  }
+
+  if (argv[3]) // Manejar número de CPUs
+  {
+    n_cpu = atoi(argv[3]);
+  }
+  else
+  {
+    n_cpu = 1;
+  }
+
+  run(argv[1], argv[2], n_cpu);
+
   return 0;
 }
