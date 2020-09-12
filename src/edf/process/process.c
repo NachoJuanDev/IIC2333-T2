88c282170c6e6d3cabb259a3d77a3ec8c3b08308
nacho11 <jisalas1@uc.cl>
2020-09-12 14:05:48 -0300

    :sparkles: Add behavior and free_process

--- src/edf/process/process.c
+++ src/edf/process/process.c
@@ -4,14 +4,32 @@
 
 #include "process.h"
 
-Process *process_init(pid_t pid, char *name, int priority, int state)
+Process *process_init(pid_t pid, char *name, int deadline, unsigned int start_time, int state)
 {
   Process *process = malloc(sizeof(Process));
 
   process->pid = pid;
   strcpy(process->name, name);
-  process->priority = priority;
+  process->deadline = deadline;
+  process->start_time = start_time;
   process->state = state;
+  process->cpu = -1;
 
   return process;
+}
+
+void process_add_behavior(Process *process,unsigned int n_behavior,unsigned int *behavior)
+{
+  process->n_behavior = n_behavior;
+  process->behavior = malloc(sizeof(unsigned int) * n_behavior);
+  for (int i = 0; i < n_behavior; i++)
+  {
+    process->behavior[i] = behavior[i];
+  }
+}
+
+void free_process(Process *process)
+{
+  free(process->behavior);
+  free(process);
 }
--- src/edf/process/process.h
+++ src/edf/process/process.h
@@ -13,12 +13,24 @@ struct process
   char name[32];
 
   /** Prioridad del proceso */
-  int priority;
+  int deadline;
+
+  /** Tiempo de llegada */
+  unsigned int start_time;
 
   /** Estado del proceso */
   int state;
 
-  /** Más atributos */
+  /** CPU */
+  int cpu;
+
+  /** Número de comportamientos */
+
+  unsigned int n_behavior;
+
+  /** Comportamiento del proceso */
+  unsigned int *behavior;
+
 };
 
 typedef enum state
@@ -30,4 +42,10 @@ typedef enum state
 } State;
 
 /** Inicia un proceso */
-Process *process_init(pid_t pid, char *name, int priority, int state);
+Process *process_init(pid_t pid, char *name, int deadline, unsigned int start_time, int state);
+
+/** Añade los comportamientos a un proceso */
+void process_add_behavior(Process *process, unsigned int n_behavior, unsigned int *behavior);
+
+/** Libera la memoria utilizada por un proceso */
+void free_process(Process *process);
